4764b04f315d4d220e87d6f15bb170cf7bce5dac
cheng <cheng.cct@outlook.com>
2021-12-01 10:04:22 +0800

    每日一题

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
-a = 10
-b = 0
-try:
-    c = a / b
-    print(c)
-except Exception as e:
-    print(e)
-print("done")
+def fib():
+    prev, curr = 0, 1
+    while True:
+        yield curr
+        curr, prev = prev + curr, curr
+
+
+if __name__ == '__main__':
+    f = fib()
+    for i in range(10):
+        print(next(f))
